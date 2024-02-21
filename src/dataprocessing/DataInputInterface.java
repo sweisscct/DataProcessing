a7f37716931b6b07f3764e5e1dd51c773b325267
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2024-02-21 20:10:00 +0000

    Made a DataInputInterface
    getAndValidateAllData method
    Started coding business logic in main

---
+++ src/dataprocessing/DataInputInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dataprocessing;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author celeb
+ */
+public interface DataInputInterface {
+    public String getLine();
+    public ArrayList<String> getAndValidateAllData();
+    
+}
