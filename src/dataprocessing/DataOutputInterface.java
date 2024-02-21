73f60f9d6da61f613a7a15c121ea00237348e15d
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2024-02-21 20:16:32 +0000

    Created DataOutputInterface

---
+++ src/dataprocessing/DataOutputInterface.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dataprocessing;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author celeb
+ */
+public interface DataOutputInterface {
+    public void outputAllData(ArrayList<String> data);
+}
