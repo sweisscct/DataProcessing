db35093ddf8de23cc023a8f8a08cc4d06a7d4e58
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2024-02-21 20:36:13 +0000

    ConsoleInput implementation, made DummyValidator

---
+++ src/dataprocessing/ConsoleInput.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataprocessing;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author celeb
+ */
+public class ConsoleInput implements DataInputInterface {
+    public ArrayList<String> getAndValidateAllData() {
+        ArrayList<String> data = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        DataValidationInterface validator;
+        
+        boolean moreData = true;
+        do {
+            System.out.println("Please enter in some data:\n");
+            String line = sc.nextLine();
+            if (validator.validateLine(line)) {
+                data.add(line);
+            }
+        } while (moreData);
+        return data;
+    }
+}
